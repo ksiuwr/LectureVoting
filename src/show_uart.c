5be73af8d14bf25f9e93695430654a7a3e31065d
R. Kaleta <rafkaleta@onet.pl>
2020-02-27 23:58:50 +0100

    Enable or disable uart

--- src/show_uart.c
+++ src/show_uart.c
@@ -1,6 +1,8 @@
 #include "show_uart.h"
 #include "memory.h"
 
+#define SEND_VIA_UART 1
+
 void show_one_vote_(character_t sign, number_t value)
 {
     uart_write(sign);
@@ -9,29 +11,38 @@ void show_one_vote_(character_t sign, number_t value)
 
 void show_uart_vote(character_t sign, number_t value)
 {
-    show_one_vote_(sign, value);
-    uart_write('\r');
-    uart_write('\n');
+    if(SEND_VIA_UART)
+    {
+        show_one_vote_(sign, value);
+        uart_write('\r');
+        uart_write('\n');
+    }
 }
 
 void show_uart_all()
 {
-    const memory * m = mem_get();
+    if(SEND_VIA_UART)
+    {
+        const memory * m = mem_get();
 
-    show_one_vote_(SIGN_PLUS, m->plus);
-    uart_write(' ');
-    show_one_vote_(SIGN_EGAL, m->egal);
-    uart_write(' ');
-    show_one_vote_(SIGN_MINUS, m->minus);
-    uart_write('\r');
-    uart_write('\n');
+        show_one_vote_(SIGN_PLUS, m->plus);
+        uart_write(' ');
+        show_one_vote_(SIGN_EGAL, m->egal);
+        uart_write(' ');
+        show_one_vote_(SIGN_MINUS, m->minus);
+        uart_write('\r');
+        uart_write('\n');
+    }
 }
 
 void show_uart_error()
 {
-    uart_write(SignError[0]);
-    uart_write(SignError[1]);
-    uart_write(SignError[2]);
-    uart_write('\r');
-    uart_write('\n');
+    if(SEND_VIA_UART)
+    {
+        uart_write(SignError[0]);
+        uart_write(SignError[1]);
+        uart_write(SignError[2]);
+        uart_write('\r');
+        uart_write('\n');
+    }
 }
